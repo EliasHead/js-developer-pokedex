71bf78242b861e09fa830505f4864918a19784ae
eliashead <eliashead.costa@gmail.com>
2023-04-13 08:48:55 -0300

    feat: details pokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,20 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
 
+const button = document.getElementsByTagName("button")
+const modal = document.querySelector("dialog")
+
+button.onclick = () => {
+   console.log("a");
+}
+
 const maxRecords = 151
 const limit = 10
 let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
+        <li class="pokemon ${pokemon.type}" onclick="selectPokemon(${pokemon.number})">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
@@ -21,6 +28,63 @@ function convertPokemonToLi(pokemon) {
             </div>
         </li>
     `
+}
+
+const selectPokemon = async (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+    const res = await fetch(url)
+    const pokemon = await res.json()
+    displayPopup(pokemon)
+}
+
+const displayPopup = (pokemon) => {
+    const imagem = pokemon.sprites.other.dream_world.front_default
+    const types = pokemon.types.map((typeSlot) => typeSlot.type.name)    
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    const htmlString = `
+        <dialog>
+        <div class="util">
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.id}</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+                </div>
+                <img id="pokemon-img" src="${imagem}"
+                 alt="${pokemon.name}"
+                >
+                <div id="data">
+                    <h4>Base stats</h4>
+                    <div id="stats">
+                    <div class="bar-inner">${pokemon.stats.map((name_stats)=>`<p class="${type}">${name_stats.stat.name}</p>`).join('')}</div>
+                    <div class="stat-desc">${pokemon.stats.map((base_stats)=>`<p class="${type}">${base_stats.base_stat}</p>`).join('')}</div>
+                </div>
+                <div class="stats-hw">
+                    <p class="${type}">Height:${(pokemon.height/10).toFixed(2)} m</p>
+                    <p class="${type}">Weight:${(pokemon.weight/10)} kg</p>
+                </div>
+                <div class="close">
+                    <button>fechar</button>
+                </div> 
+            </li>
+        </div>
+        </dialog>
+    `
+    pokemonList.innerHTML = htmlString + pokemonList.innerHTML
+
+    const popup = document.querySelector('dialog')
+    popup.showModal()
+
+    const buttonClose = document.querySelector('dialog button') 
+    buttonClose.onclick = function() {
+        popup.close()
+    }   
 }
 
 function loadPokemonItens(offset, limit) {
